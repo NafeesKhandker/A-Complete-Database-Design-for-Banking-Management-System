3cd2f84241d20b8e5fa7b84e08bedec77a58db66
Khandker M Qaiduzzaman <nafees.ku11@gmail.com>
2020-01-18 23:34:42 +0100

    Create UserMainWindow.xaml.cs

---
+++ Bankmanagment/UserMainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bankmanagment
+{
+    /// <summary>
+    /// Interaction logic for UserMainWindow.xaml
+    /// </summary>
+    public partial class UserMainWindow : Window
+    {
+        public UserMainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
