f37a84653f86c927cd9e3a2acfa9ae0f3d949b12
Khandker M Qaiduzzaman <nafees.ku11@gmail.com>
2020-01-18 23:14:40 +0100

    Create EmployeWindow.xaml.cs

---
+++ Bankmanagment/Emloyee/EmployeWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bankmanagment.Emloyee
+{
+    /// <summary>
+    /// Interaction logic for EmployeWindow.xaml
+    /// </summary>
+    public partial class EmployeWindow : Window
+    {
+        public EmployeWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
