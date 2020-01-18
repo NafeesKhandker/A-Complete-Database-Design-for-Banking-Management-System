1de79e818a5e35b0d0e027c64181d1bd3916f2b4
Khandker M Qaiduzzaman <nafees.ku11@gmail.com>
2020-01-18 23:34:33 +0100

    Create UserControl4.xaml.cs

---
+++ Bankmanagment/UserControl4.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bankmanagment
+{
+    /// <summary>
+    /// Interaction logic for UserControl4.xaml
+    /// </summary>
+    public partial class UserControl4 : UserControl
+    {
+        public UserControl4()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            grid1.Children.Clear();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+
+             UserControl2 u2 = new UserControl2();
+            grid1.Children.Clear();
+            grid1.Children.Add(u2);
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
