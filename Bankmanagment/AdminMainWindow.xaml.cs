6db16335ea873e9d21f205937fc26045d467d020
Khandker M Qaiduzzaman <nafees.ku11@gmail.com>
2020-01-18 23:09:34 +0100

    Create AdminMainWindow.xaml.cs

---
+++ Bankmanagment/AdminMainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bankmanagment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow1.xaml
+    /// </summary>
+    public partial class AdminMainWindow : Window
+    {
+
+        public AdminMainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+      
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            ShowAll u = new ShowAll();
+            grid1.Children.Clear();
+            grid1.Children.Add(u);
+           
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            
+            grid1.Children.Clear();
+         
+
+        }
+
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e){
+          
+            grid1.Children.Clear();
+            MainWindow m = new MainWindow();
+            m.Show();
+            this.Close();
+           
+       
+        }
+
+      
+
+        private void MenuItem_Click_4(object sender, RoutedEventArgs e)
+        {
+            CreateAccount c = new CreateAccount();
+            grid1.Children.Clear();
+            grid1.Children.Add(c);
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
+
+    }
+}
